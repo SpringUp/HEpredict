a58d3e7b5f225b2981de4aa5ed83d001b830f06c
SpringUp <1743388711@qq.com>
2023-12-19 12:02:17 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,8 +0,0 @@
-source /home/ubuntu/anaconda3/etc/profile.d/conda.sh
-
-source activate pytorch_p39
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install tensorboard
-
-python main_sync_wb.py --local_rank 0 --classification 2 --K 5 --cnv /mnt/efs/fs1/code/tmbclinic/clinical_with_necessary_columns.csv --model resnet18
-
